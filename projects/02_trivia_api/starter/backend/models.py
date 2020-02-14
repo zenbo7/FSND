db42c2986db5b88122bcf89042e0dffe848a88a6
matteocattaneo <matteocattaneo74@gmail.com>
2020-02-14 09:59:28 +0100

    added testfile

--- projects/02_trivia_api/starter/backend/models.py
+++ projects/02_trivia_api/starter/backend/models.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 import json
 
 database_name = "trivia"
-database_path = "postgres://{}:{}@{}/{}".format('matteocattaneo', 'matt4776','localhost:5432', database_name)
+database_path = "postgresql://{}:{}@{}/{}".format('matteocattaneo', 'matt4776','localhost:5432', database_name)
 
 db = SQLAlchemy()
 
